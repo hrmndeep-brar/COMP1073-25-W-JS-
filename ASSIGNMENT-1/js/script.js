9ae320e4956c7da42d4539be94b4994e2ebd0cf8
hrmndeep-brar <harmandeepkaur20069@gmail.com>
2025-02-20 18:20:59 -0500

    linked js to the html and added random names, places, actions to create a story

--- ASSIGNMENT-1/js/script.js
+++ ASSIGNMENT-1/js/script.js
@@ -1,11 +1,11 @@
 // Array of word arrays for each colored button (column)
 // The first array corresponds to the purple button, the second to blue, and so on.
 const words = [
-    ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"],
-    ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed"],
-    ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"],
-    ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"],
-    ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoes"]
+    ["Aunt", "Hina", "Uncle John", "Anna", "My Professor", "The Tiger", "The lion"],
+    ["talked to", "travelled with", "sat on", "saw", "studied with", "hugged tightly"],
+    ["a stubborn", "a lazy", "a beautiful", "a slimy", "a messy", "an intelligent"],
+    ["duck", "ship", "horse", "cat", "unicorn", "vampire", "warewolf"],
+    ["on the jupiter", "in Paris", "in the class", "in London", "on the grass", "in Canada"]
   ];
   
 
