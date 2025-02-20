ed9c1de2268814a5f194deda3a9e138b152c77f2
hrmndeep-brar <harmandeepkaur20069@gmail.com>
2025-02-20 18:19:04 -0500

    linked js to html and added the random names, verbs, actions and places for making story.

---
+++ ASSIGNMENT-1/js/script.js
@@ -0,0 +1,12 @@
+// Array of word arrays for each colored button (column)
+// The first array corresponds to the purple button, the second to blue, and so on.
+const words = [
+    ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"],
+    ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed"],
+    ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"],
+    ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"],
+    ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoes"]
+  ];
+  
+
+  
