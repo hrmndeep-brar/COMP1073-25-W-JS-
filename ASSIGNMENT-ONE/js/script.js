749b3287494030e57d780920486e146e5d651576
hrmndeep-brar <harmandeepkaur20069@gmail.com>
2025-02-20 20:54:52 -0500

    Wrote different names, places and animals to create random story

---
+++ ASSIGNMENT-ONE/js/script.js
@@ -0,0 +1,9 @@
+// Array of word arrays for each colored button (column)
+// The first array corresponds to the purple button, the second to blue, and so on.
+const words = [
+    ["Aunt", "Hina", "Uncle John", "Anna", "My Professor", "The Tiger", "The lion"],
+    ["talked to", "travelled with", "sat on", "saw", "studied with", "hugged tightly"],
+    ["a stubborn", "a lazy", "a beautiful", "a slimy", "a messy", "an intelligent"],
+    ["duck", "ship", "horse", "cat", "unicorn", "vampire", "warewolf"],
+    ["on the jupiter", "in Paris", "in the class", "in London", "on the grass", "in Canada"]
+  ];
